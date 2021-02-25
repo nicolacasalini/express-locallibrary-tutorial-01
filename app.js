0b42e4b3e81bb442246c8f363aad39ff12a00bc4
nicolacasalini <casalininicola@gmail.com>
2021-02-25 12:26:36 +0100

    Added a Procfile.

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 //var mongoDB = 'mongodb+srv://nicola:nikkola57@cluster0-zwexd.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
-var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+//var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var dev_db_url = 'mongodb+srv://nicola:nikkola57@cluster0-zwexd.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+//var dev_db_url = 'mongodb+srv://nicola:nikkola57@cluster0-zwexd.gcp.mongodb.net/local_library_01?retryWrites=true&w=majority';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 // Set up mongoose connection
